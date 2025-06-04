07ba0ecb8510d9c8475fff63cb160303362045a0
redstone59 <redstonepro23@googlemail.com>
2025-06-04 23:04:37 +1000

    pretty up preprocessor errors

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -54,9 +54,49 @@ impl Preprocessor {
         self.index >= self.max
     }
 
+    fn get_surrounding_lines(&self, index: usize) -> String {
+        let lines: Vec<String> = self.source
+                                     .lines()
+                                     .map(|x| x.to_string())
+                                     .collect();
+        
+        let mut start_line = index - 2;
+        if start_line < 0 { start_line = 0; }
+
+        let mut end_line = index + 2;
+        if end_line >= self.max { end_line = self.max - 1 }
+
+        let mut to_display: Vec<String> = vec![];
+
+        if start_line != 0 { to_display.push("   | ...".to_string()); }
+        for i in start_line..=end_line {
+            let to_push: String = format!("{:>3}: {}", i, lines[i]);
+            if i == index {
+                to_display.push(format!("\x1b[1;33m{}\x1b[0m", to_push));
+            } else {
+                to_display.push(to_push);
+            }
+        }
+        if end_line != self.max { to_display.push("   | ...".to_string()); }
+        
+        to_display.join("\n")
+    }
+
+    fn err_string(&self, message: String, line_index: usize) -> String {
+        format!(
+            // '\x1b[1;31m' sets text to bold red.
+            // '\x1b[0m' sets text to terminal default.
+            "=====\n{}\n=====\n\x1b[1;31mError in preprocessing\x1b[0m: {}\n=====",
+            self.get_surrounding_lines(line_index),
+            message
+        )
+    }
+
     fn match_block(&mut self) -> String {
+        // Starts at 1 since a '{' was consumed before this was called.
         let mut bracket_level: i32 = 1;
         let start_index: usize = self.index;
+        let start_line: usize = self.line;
 
         while bracket_level != 0 {
             if let Some(current) = self.next() {
@@ -71,9 +111,15 @@ impl Preprocessor {
             
             if self.is_at_end() {
                 panic!(
-                    "Mismatched brackets for block starting at line {}, character {} to end of file.",
-                    self.line,
-                    start_index
+                    "{}",
+                    self.err_string(
+                        format!(
+                            "Mismatched brackets for block starting at line {}, character {} to end of file.",
+                            start_line,
+                            start_index
+                        ),
+                        start_line
+                    )
                 );
             }
         }
